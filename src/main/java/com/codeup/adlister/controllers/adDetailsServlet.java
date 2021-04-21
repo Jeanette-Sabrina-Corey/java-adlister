316bfcb60722d5b7bf83acd8a8c37de708676fc1
Corey Koontz <corey.michael.koontz@gmail.com>
2021-04-20 23:09:22 -0500

    Adding completed adDetails servlet. Waiting on commiting the getAdbyId method until morning but have that is completed too

---
+++ src/main/java/com/codeup/adlister/controllers/adDetailsServlet.java
@@ -0,0 +1,28 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet ("ads/detailed")
+public class adDetailsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long id = Long.parseLong(req.getParameter("id"));// id for button has to be ${Ad.id}
+
+        // - Storing the ad being selected in this req obj so it can be displayed in the adDetails.jsp
+        Ad ad = DaoFactory.getAdsDao().getAdById(id); // - Need to create a getAdById method
+        req.setAttribute("ad", ad);                //   in the interface and define it
+                                                      //   in the MySQLAdsDao
+        req.getRequestDispatcher("/WEB-INF/detailed.jsp").forward(req, resp);
+    }
+
+
+}
